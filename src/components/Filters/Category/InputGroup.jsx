207b0fef931b074d41944401b8aa713c58c7a8a8
Thiago Silva <thiagowfer@gmail.com>
2023-02-24 19:59:00 -0300

    finish location page

--- src/components/Filters/Category/InputGroup.jsx
+++ src/components/Filters/Category/InputGroup.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
 
 const InputGroup = ({ setId, name, total }) => {
-
-  console.log()
 
   return (
     <div class="input-group mb-3">
         <select onChange={e => setId(e.target.value)} class="form-select" id={name} >
-            <option selected> Choose... </option>
+            <option value="1" selected> Choose... </option>
                 {[...Array(total).keys()].map((x) => {
                     return (
                         <option value={x + 1}> 
