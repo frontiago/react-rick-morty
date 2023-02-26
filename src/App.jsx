c65eb6d941eba79cc980094204e6a3efc324e0ec
Thiago Silva <thiagowfer@gmail.com>
2023-02-26 00:00:21 -0300

    fix page path

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import CardDetails from './components/Cards/CardDetails'
 
-import Characters from './pages/Characters'
-import Episodes from './pages/Episodes'
-import Location from './pages/Location'
+import Characters from './Pages/Characters'
+import Episodes from './Pages/Episodes'
+import Location from './Pages/Location'
 
 function App() {
   return (
